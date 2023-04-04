a6f0acd4fde768c9bdbf17bf9321bbfc1cbff5f6
Adrien <adriennavarre@hotmail.com>
2023-04-04 00:47:54 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
